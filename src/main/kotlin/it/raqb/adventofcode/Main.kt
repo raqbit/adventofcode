0145f9527b681c6080ebe45e484bd9f38ad3fb13
Raqbit <raqbit@outlook.com>
2017-12-01 22:16:02 +0100

    Add color lib for fancy menu

--- src/main/kotlin/it/raqb/adventofcode/Main.kt
+++ src/main/kotlin/it/raqb/adventofcode/Main.kt
@@ -1,5 +1,9 @@
 package it.raqb.adventofcode
 
+import com.andreapivetta.kolor.green
+import com.andreapivetta.kolor.lightGray
+import com.andreapivetta.kolor.lightWhite
+import com.andreapivetta.kolor.yellow
 import it.raqb.adventofcode.day1.Day1
 
 val year = "2017"
@@ -8,17 +12,17 @@ val days = arrayOf(Day1)
 
 fun main(args: Array<String>) {
 
-    println("==== AdventOfCode $year ====")
+    println("==== AdventOfCode $year ====".green())
     for ((index, day) in days.withIndex()) {
-        println("Day ${index + 1}: ${day.name}  ${"*".repeat(day.starCount)}")
+        println("Day ${index + 1}: ${day.name}  ${getStars(day.starCount)}")
     }
-    println("===========================")
+    println("=== Solutions by Raqbit ===".green())
     println()
 
     var selectedDayNum: Int
 
-    while (true){
-        print("Enter a valid day: ")
+    while (true) {
+        print("Enter a day number: ")
 
         val inputString = readLine().orEmpty()
 
@@ -29,7 +33,7 @@ fun main(args: Array<String>) {
             continue
         }
 
-        if(selectedDayNum < 1 || selectedDayNum > days.size){
+        if (selectedDayNum < 1 || selectedDayNum > days.size) {
             println("That day doesn't exist (yet)")
             continue
         }
@@ -43,4 +47,12 @@ fun main(args: Array<String>) {
     println()
 
     selectedDay.exe()
+}
+
+fun getStars(starCount: Int): String {
+    return when (starCount) {
+        2 -> "*".yellow()
+        1 -> "*"
+        else -> ""
+    }
 }
