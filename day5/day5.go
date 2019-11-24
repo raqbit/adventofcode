b9685e5cfdb3264369235bb7be29b29580a76289
Raqbit <raqbit@outlook.com>
2019-11-24 18:16:08 +0100

    Part2 down to 6ms by using part1 result in part2

--- day5/day5.go
+++ day5/day5.go
@@ -14,16 +14,18 @@ func main() {
 		panic("Could not load input")
 	}
 
-	part1(input)
+	result := part1(input)
 	fmt.Println("----")
-	part2(input)
+	// Using the result of part1 in part2 because it has already been done reacting without removing characters
+	part2(result)
 }
 
-func part1(input string) {
+func part1(input string) string {
 	result := react(input)
 
 	fmt.Printf("Final string: %s\n", result)
 	fmt.Printf("Length: %d\n", len(result))
+	return result
 }
 
 func part2(input string) {
@@ -56,7 +58,7 @@ func react(input string) string {
 	stack := make([]rune, 0)
 
 	for _, currChar := range input {
-		n := int32(len(stack) - 1) // Top element
+		n := int32(len(stack) - 1) // Top char
 
 		if n < 0 {
 			stack = append(stack, currChar)
