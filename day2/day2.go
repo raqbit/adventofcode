7a4d43820417441d7fa836419ee993231ff28205
Raqbit <raqbit@outlook.com>
2019-12-05 14:11:12 +0100

    Day 5!

--- day2/day2.go
+++ day2/day2.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"raqb.it/AdventOfCode/shared"
-	"strconv"
-	"strings"
+	"raqb.it/AdventOfCode/shared/intcode"
 )
 
 func main() {
@@ -14,50 +13,20 @@ func main() {
 		panic("Could not load input")
 	}
 
-	intcodes, err := parseIntcodes(input)
+	intcodes, err := intcode.ParseIntcodes(input)
 
 	if err != nil {
 		panic("Could not parse input")
 	}
 
-	ic := shared.NewIntComputer()
-
-	ic.RegisterInstruction(&shared.Instruction{
-		Name:   "ADD",
-		Opcode: 1,
-		ArgC:   3,
-		Execute: func(c *shared.IntComputer, argv []int) {
-			c.Memory[argv[2]] = c.Memory[argv[0]] + c.Memory[argv[1]]
-			fmt.Printf("$%d + $%d => $%d\n", argv[2], argv[0], argv[1])
-		},
-	})
-
-	ic.RegisterInstruction(&shared.Instruction{
-		Name:   "MULT",
-		Opcode: 2,
-		ArgC:   3,
-		Execute: func(c *shared.IntComputer, argv []int) {
-			c.Memory[argv[2]] = c.Memory[argv[0]] * c.Memory[argv[1]]
-			fmt.Printf("$%d * $%d => $%d\n", argv[2], argv[0], argv[1])
-		},
-	})
-
-	ic.RegisterInstruction(&shared.Instruction{
-		Name:   "HALT",
-		Opcode: 99,
-		ArgC:   0,
-		Execute: func(c *shared.IntComputer, argv []int) {
-			c.FlagHalt()
-			fmt.Println("HALT")
-		},
-	})
+	ic := intcode.NewIntComputer()
 
 	part1(ic, intcodes)
 	fmt.Println("----")
 	part2(ic, intcodes)
 }
 
-func part1(ic *shared.IntComputer, instructions []int) {
+func part1(ic *intcode.Computer, instructions []int) {
 	ic.SetInitialMemory(instructions)
 	ic.SetNoun(12)
 	ic.SetVerb(2)
@@ -72,7 +41,7 @@ func part1(ic *shared.IntComputer, instructions []int) {
 	ic.Reset()
 }
 
-func part2(ic *shared.IntComputer, instructions []int) {
+func part2(ic *intcode.Computer, instructions []int) {
 AllInputs:
 	for noun := 0; noun <= 99; noun++ {
 		for verb := 0; verb <= 99; verb++ {
@@ -96,60 +65,4 @@ AllInputs:
 			ic.Reset()
 		}
 	}
-}
-
-func runProgram(instructions []int) []int {
-	programMemory := make([]int, len(instructions))
-	copy(programMemory, instructions)
-
-	ip := 0
-run:
-	for {
-		// Opcode
-		opcode := programMemory[ip]
-
-		var param1 int
-		var param2 int
-
-		// HALT opcode has no params
-		if ip+3 < len(programMemory) {
-			// First input parameter
-			param1 = programMemory[programMemory[ip+1]]
-			param2 = programMemory[programMemory[ip+2]]
-		}
-
-		// Output address
-		outputAddr := programMemory[ip+3]
-
-		switch opcode {
-		case 1:
-			// Opcode: ADD
-			programMemory[outputAddr] = param1 + param2
-		case 2:
-			// Opcode: MULT
-			programMemory[outputAddr] = param1 * param2
-		case 99:
-			// Opcode: HALT
-			break run
-		}
-
-		// Move instruction pointer
-		ip += 4
-	}
-
-	return programMemory
-}
-
-// Parses the input lines as integers
-func parseIntcodes(input string) ([]int, error) {
-	unparsedIntcodes := strings.Split(strings.TrimSpace(input), ",")
-	intcodes := make([]int, len(unparsedIntcodes))
-	for i, uic := range unparsedIntcodes {
-		code, err := strconv.ParseInt(uic, 10, 32)
-		if err != nil {
-			return nil, err
-		}
-		intcodes[i] = int(code)
-	}
-	return intcodes, nil
 }
---
+++ shared/intcode/computer.go
@@ -0,0 +1,261 @@
+package intcode
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type ParamMode int
+
+const (
+	Position  ParamMode = 0
+	Immediate ParamMode = 1
+)
+
+type Instruction struct {
+	Name    string
+	paramC  int
+	Execute func(c *Computer, modes []ParamMode, params []int)
+}
+
+type Computer struct {
+	ip           int
+	Memory       []int
+	instructions map[int]*Instruction
+	halt         bool
+	err          error
+}
+
+func (c *Computer) GetValue(params []ParamMode, i int, value int) int {
+	paramMode := Position
+
+	if i < len(params) {
+		paramMode = params[i]
+	}
+
+	switch paramMode {
+	case Immediate:
+		return value
+	case Position:
+		return c.Memory[value]
+	default:
+		fmt.Println("Error: Unknown param mode, returning 0")
+	}
+
+	return 0
+}
+
+func (c *Computer) Start() error {
+	for {
+		// Parse instruction at instruction pointer into opcode + params
+		opcode, paramModes := parseInstruction(c.Memory[c.ip])
+
+		// Find relevant instruction executor
+		inst, ok := c.instructions[opcode]
+		if !ok {
+			return errors.New(fmt.Sprintf("invalid instruction: %d", opcode))
+		}
+
+		// Save instruction pointer
+		previousIp := c.ip
+
+		// Get parameters for instruction
+		params := c.Memory[c.ip+1 : c.ip+inst.paramC+1]
+
+		// Execute instruction
+		inst.Execute(c, paramModes, params)
+
+		// If error is set, halt & print error
+		if c.err != nil {
+			return fmt.Errorf("error while executing %s instruction: %w", inst.Name, c.err)
+		}
+
+		// If halt flag is set, halt
+		if c.halt {
+			break
+		}
+
+		// Only increment instruction pointer if it has not been modified
+		if c.ip == previousIp {
+			// Increment by one plus the argument count of the instruction
+			c.ip += 1 + inst.paramC
+		}
+
+	}
+
+	return nil
+}
+
+func parseInstruction(inst int) (int, []ParamMode) {
+	instStr := strconv.Itoa(inst)
+
+	// Get lower two digits
+	opcode := inst % 100
+
+	paramModes := make([]ParamMode, 0)
+
+	if len(instStr) > 2 {
+		// Get lower two digits
+		instStr = instStr[:len(instStr)-2]
+
+		for i := len(instStr) - 1; i >= 0; i-- {
+			// Removing 0 ('1' - '0' == 1)
+			paramModes = append(paramModes, ParamMode(instStr[i]-'0'))
+		}
+	}
+
+	return opcode, paramModes
+}
+
+func (c *Computer) RegisterInstruction(opcode int, inst *Instruction) {
+	c.instructions[opcode] = inst
+}
+
+func (c *Computer) SetInitialMemory(initMem []int) {
+	c.Memory = make([]int, len(initMem))
+	copy(c.Memory, initMem)
+}
+
+func (c *Computer) SetNoun(noun int) {
+	c.Memory[1] = noun
+}
+
+func (c *Computer) SetVerb(verb int) {
+	c.Memory[2] = verb
+}
+
+func (c *Computer) FlagHalt() {
+	c.halt = true
+}
+
+func (c *Computer) Reset() {
+	c.Memory = nil
+	c.ip = 0
+	c.halt = false
+}
+
+func NewIntComputer() *Computer {
+	ic := &Computer{instructions: make(map[int]*Instruction)}
+
+	ic.RegisterInstruction(1, &Instruction{
+		Name:   "ADD",
+		paramC: 3,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			c.Memory[params[2]] = param1 + param2
+		},
+	})
+
+	ic.RegisterInstruction(2, &Instruction{
+		Name:   "MUL",
+		paramC: 3,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			c.Memory[params[2]] = param1 * param2
+		},
+	})
+
+	ic.RegisterInstruction(99, &Instruction{
+		Name:   "HLT",
+		paramC: 0,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			c.FlagHalt()
+		},
+	})
+
+	ic.RegisterInstruction(4, &Instruction{
+		Name:   "OUT",
+		paramC: 1,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			fmt.Printf("OUTPUT: %d\n", c.GetValue(modes, 0, params[0]))
+		},
+	})
+
+	ic.RegisterInstruction(3, &Instruction{
+		Name:   "IN",
+		paramC: 1,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			fmt.Print("INPUT: ")
+
+			// Read input
+			var input string
+			_, err := fmt.Scanln(&input)
+			if err != nil {
+				c.err = err
+				return
+			}
+
+			// Parse input
+			i, err := strconv.ParseInt(input, 10, 32)
+			if err != nil {
+				c.err = err
+				return
+			}
+
+			c.Memory[params[0]] = int(i)
+		},
+	})
+
+	ic.RegisterInstruction(5, &Instruction{
+		Name:   "JNZ",
+		paramC: 2,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			if param1 != 0 {
+				c.ip = param2
+			}
+		},
+	})
+
+	ic.RegisterInstruction(6, &Instruction{
+		Name:   "JZ",
+		paramC: 2,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			if param1 == 0 {
+				c.ip = param2
+			}
+		},
+	})
+
+	ic.RegisterInstruction(7, &Instruction{
+		Name:   "LT",
+		paramC: 3,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			if param1 < param2 {
+				c.Memory[params[2]] = 1
+			} else {
+				c.Memory[params[2]] = 0
+			}
+		},
+	})
+
+	ic.RegisterInstruction(8, &Instruction{
+		Name:   "EQ",
+		paramC: 3,
+		Execute: func(c *Computer, modes []ParamMode, params []int) {
+			param1 := c.GetValue(modes, 0, params[0])
+			param2 := c.GetValue(modes, 1, params[1])
+
+			if param1 == param2 {
+				c.Memory[params[2]] = 1
+			} else {
+				c.Memory[params[2]] = 0
+			}
+		},
+	})
+
+	return ic
+}
