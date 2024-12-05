e996565896bc728a10cceae5454d333ca89737d9
Raqbit <raqbit@outlook.com>
2024-12-05 21:34:33 +0100

    Day 4 pt 1, working well enough for full input
    There's a workaround for an issue that I don't want to spend time fixing,
    which works well enough for the full input

--- day4/__main__.py
+++ day4/__main__.py
@@ -4,45 +4,46 @@ from shared import read_input
 
 
 def _count_word_occurrences(crossword: str, word: str) -> int:
+    """
+    Note: This has a known bug in the edge case that any word searched ends
+    """
     row_len = crossword.find("\n") + 1
 
     total = 0
     for pos in range(len(crossword)):
-        x_pos = pos % row_len
-        y_pos = pos // row_len
         if crossword[pos] != word[0]:
             continue
 
         # Horizontal
-        if crossword[pos : pos + 4] == word:
+        if crossword[pos : pos + len(word)] == word:
             total += 1
 
         # Horizontal reverse
-        if crossword[pos : pos - 4 : -1] == word:
+        if crossword[pos : max(pos - len(word), 0) : -1] == word:
             total += 1
 
         # Vertical down
-        if crossword[pos : pos + row_len * 4 : row_len] == word:
+        if crossword[pos : pos + row_len * len(word) : row_len] == word:
             total += 1
 
         # Vertical up
-        if crossword[pos : pos - (row_len * 4) : -row_len] == word:
+        if crossword[pos : max(pos - (row_len * len(word)), 0) : -row_len] == word:
             total += 1
 
         # Diagonal down right
-        if crossword[pos : pos + row_len * 4 : row_len + 1] == word:
+        if crossword[pos : pos + row_len * len(word) : row_len + 1] == word:
             total += 1
 
         # Diagonal down left
-        if crossword[pos : pos + row_len * 3 : row_len - 1] == word:
+        if crossword[pos : pos + row_len * (len(word) - 1) : row_len - 1] == word:
             total += 1
 
         # Diagonal up left
-        if crossword[pos : pos - (row_len * 4) : -(row_len + 1)] == word:
+        if crossword[pos : max(pos - (row_len * len(word)), 0) : -(row_len + 1)] == word:
             total += 1
 
         # Diagonal up right
-        if crossword[pos : pos - (row_len * 3) : -(row_len - 1)] == word:
+        if crossword[pos : max(pos - (row_len * (len(word) - 1)), 0) : -(row_len - 1)] == word:
             total += 1
 
     return total
